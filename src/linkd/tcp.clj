ff513ca9c168f6c6193237728192446927b922e1
wawuta <wawuta@gmail.com>
2014-06-21 00:37:36 +0800

    add tcp client

--- src/linkd/tcp.clj
+++ src/linkd/tcp.clj
@@ -3,11 +3,14 @@
   (:import [java.net InetSocketAddress])
   (:import [java.util.concurrent Executors])
   (:import [org.jboss.netty.bootstrap
+            ClientBootstrap
             ServerBootstrap])
   (:import [org.jboss.netty.channel
             Channels
             ChannelPipelineFactory])
-  (:import [org.jboss.netty.channel.socket.nio NioServerSocketChannelFactory]))
+  (:import [org.jboss.netty.channel.socket.nio
+            NioServerSocketChannelFactory
+            NioClientSocketChannelFactory]))
 
 (defn- create-pipeline [handler encoder decoder]
   (reify ChannelPipelineFactory
@@ -19,3 +22,42 @@
           (.addLast pipeline "encoder" (netty-encoder encoder)))
         (.addLast pipeline "handler" handler)
         pipeline))))
+
+(defn- start-tcp-server [port handler encoder decoder boss-pool worker-pool]
+  (let [factory (NioServerSocketChannelFactory. boss-pool worker-pool)
+        bootstrap (ServerBootstrap. factory)
+        pipeline (create-pipeline handler encoder decoder)]
+    (.setPipelineFactory bootstrap pipeline)
+    (.bind bootstrap (InetSocketAddress. port))))
+
+(defn tcp-server [port handler
+                  &{:keys [encoder decoder codec boss-pool worker-pool]
+                    :or {encoder nil
+                         decoder nil
+                         codec nil
+                         boss-pool (Executors/newCachedThreadPool)
+                         worker-pool Executors/newCachedThreadPool)}}]
+  (let [encoder (or encoder codec)
+        decoder (or decoder codec)]
+    (start-tcp-server port handler encoder decoder boss-pool worker-pool)))
+
+(defn tcp-client [host port handler
+                  &{:keys [encoder decoder codec boss-pool worker-pool]
+                    :or {encoder nil
+                         decoder nil
+                         codec nil
+                         boss-pool (Executors/newCachedThreadPool)
+                         worker-pool (Executors/newCachedThreadPool)}}]
+  (let [encoder (or encoder codec)
+        decoder (or decoder codec)
+        bootstrap (ClientBootstrap.
+                    (NioClientSocketChannelFactory. boss-pool worker-pool))
+        pipeline (create-pipeline handler encoder decoder)]
+    (.setPipelineFactory bootstrap pipeline)
+    (.. (.connect bootstrap (InetSocketAddress. host port))
+      awaitUninterruptibly
+      getChannel)))
+
+
+
+
