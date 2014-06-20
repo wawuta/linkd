7c91500f4b71de9993a23a698a6ccd523ef059fd
wawuta <wawuta@gmail.com>
2014-06-20 22:06:32 +0800

    add header codec

--- project.clj
+++ project.clj
@@ -1,4 +1,6 @@
-(defproject linkd "1.0.0-SNAPSHOT"
-  :description "FIXME: write description"
+(defproject linkd "0.1.0-SNASHOT"
+  :description "A straightforward (not-so-clojure) clojure wrapper for java nio framework"
   :dependencies [[org.clojure/clojure "1.3.0"]
-                 [io.netty/netty "3.3.1.Final"]])
+                 [io.netty/netty "3.3.1.Final"]
+                 [org.clojure/core.match "0.2.0-alpha9"]])
+
